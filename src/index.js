bd7894ca8753051469099dfc9a80a9bd7b0d5efe
kkeeth <zensin0082@gmail.com>
2022-10-21 21:23:30 +0900

    feat: added mocks for browser and node

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import '@riotjs/hot-reload'
-import { component } from 'riot'
-import App from './app.riot'
-import registerGlobalComponents from './register-global-components'
+import "@riotjs/hot-reload";
+import { component } from "riot";
+import App from "./app.riot";
+import registerGlobalComponents from "./register-global-components";
 
 // register
-registerGlobalComponents()
+registerGlobalComponents();
+
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
 
 // mount the root tag
-component(App)(document.getElementById('root'))
+component(App)(document.getElementById("root"));
